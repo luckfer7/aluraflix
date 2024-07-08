f2b642e06e447641942cf308d4e584764e9cc8f3
luckfer7 <lucasferreira059@gmail.com>
2024-07-08 11:18:49 -0300

    responsividade de paginas

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -9,7 +9,7 @@ function Banner () {
                     <h2 className={styles.Title}>SEO COM REACT</h2>
                     <p className={styles.text}>Eu to aqui pra nesse vídeo dizer que a gente vai aprender a começar uma app inspirada no desenho Pokémon com Nextjs e React, ver algumas dicas sobre performance e de quebra conhecer uma plataforma sensacional pra fazer deploy que é a Vercel. Tudo em 22 minutos nesse vídeo feito com todo o carinho do mundo construindo uma "Pokedex"! </p>
                 </div>
-                <img src={ImgBanner} className=''></img>
+                <img className={styles.imgBanner} src={ImgBanner}></img>
             </div>
             
         </div>
